6d1c55af42d6513d849ad5df9b8416e3b79b89ed
albim72 <marcin.albiniak72@gmail.com>
2022-07-17 13:26:36 +0200

    Update User.py

--- AUKCA/User.py
+++ AUKCA/User.py
@@ -34,3 +34,16 @@ class User:
     
     def logged_user_info(self):
         print(f"użytkownik: {self.login}, hasło: {self.password}")
+        
+     def __incorrect_password(self):
+        cur = self.conDB.cursor()
+
+        for row_failed_login in cur.execute("SELECT COUNTER_FAILED_LOGIN FROM Users WHERE LOGIN = ?",[self.login]):
+            self.wrong_pass_counter = int(row_failed_login[0]) +1
+        cur.execute("UPDATE Users SET COUNTER_FAILED_LOGIN = ? WHERE LOGIN = ?",(self.wrong_pass_counter,self.login))
+        self.conDB.commit()
+        
+        if self.wrong_pass_counter > 3:
+            cur.execute("UPDATE Users SET ACC_ACTIVE = 0 WHERE LOGIN=?",[self.login])
+            self.conDB.commit()
+        print("złe hasśło lub login!")
